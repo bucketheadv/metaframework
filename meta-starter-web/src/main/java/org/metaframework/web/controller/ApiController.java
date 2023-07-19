ed301ff3b742232689af1f3cdcdaf415fb7ae1f1
Sven <sven199109@gmail.com>
2023-07-19 14:49:57 +0800

    pom优化

--- meta-starter-web/src/main/java/org/metaframework/web/controller/ApiController.java
+++ meta-starter-web/src/main/java/org/metaframework/web/controller/ApiController.java
@@ -1,5 +1,6 @@
 package org.metaframework.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.metaframework.web.dao.mapper.UserMapper;
 import org.metaframework.web.prop.BlackListProperties;
 import org.metaframework.web.service.UserService;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author sven
  * Created on 2023/7/15 3:54 PM
  */
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class ApiController {
